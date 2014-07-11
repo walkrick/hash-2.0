237b28e7bef7d789e2245369e0e147ec6bbd3399
Rick Walker <walkrick@yahoo.com>
2014-07-11 11:37:13 -0600

    solo dog

---
+++ lib/dog_hash.rb
@@ -0,0 +1,49 @@
+DOG = {
+  cattle_dog: {preferences: [{:name => "Harleigh"}],
+               names: ['Harleigh', 'brown']},
+  "harleigh" => {preferences: [{:favorite_toy => "frog"}],
+                 information:{:sport => ["frisbee", "hiking"]}
+                },
+
+
+  "trixie" => {preferences: [{:favorite_food => "steak"}],
+               information: {:sport => [:swimming, :prancing]}
+              },
+
+
+              }
+
+
+
+
+
+
+
+
+
+
+
+# DOG = {
+#   :cattle_dog => {:preferences => [{}]}
+# }
+
+
+# G = {
+#   cattle_dog:{
+#     preferences:
+#       [{name: "Harleigh"}],
+#     names: ['Harleigh','brown']
+#   },
+#   "harleigh" => {
+#     preferences: [{
+#                     favorite_toy: "frog"}],
+#     information: {
+#       sport: ["frisbee", "hiking"]}
+#   },
+#   "trixie" => {
+#     preferences: [{
+#                     favorite_food: "steak" }],
+#     information: {
+#       sport: [:swimming, :prancing]}
+#   }
+# }
